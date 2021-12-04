8c266544987988b9cd2df1e3e6dfadcbccebaeba
AndreyUser <AndreyUser@ANDREY>
2021-12-04 04:04:47 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
